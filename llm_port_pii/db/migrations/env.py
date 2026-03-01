f3efb45277dcc3aeb9f877211780c0aa58223ead
Sachi Harshitha Liyanagama <sachi.harshitha@live.com>
2026-03-01 11:08:25 +0100

    fix alembix threading issue

--- llm_port_pii/db/migrations/env.py
+++ llm_port_pii/db/migrations/env.py
@@ -79,10 +79,7 @@ async def run_migrations_online() -> None:
         await connection.run_sync(do_run_migrations)
 
 
-loop = asyncio.get_event_loop()
 if context.is_offline_mode():
-    task = run_migrations_offline()
+    asyncio.run(run_migrations_offline())
 else:
-    task = run_migrations_online()
-
-loop.run_until_complete(task)
+    asyncio.run(run_migrations_online())
